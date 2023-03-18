2eb34a823a70485f1b22329a7a830c5b8766f6ef
vis-hoo <vicente.falcon.arancibia@hotmail.com>
2023-03-18 10:11:12 -0300

    Lab 1 - Ejercicio 5 - C#

---
+++ Lab Semana 1/labsemana1_ejercicio5/labsemana1_ejercicio5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labsemana1_ejercicio5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int piezaActual;
+            int piezaAnterior = 0;
+            bool seRepite;
+
+            Console.WriteLine("Generando 20 piezas al azar.");
+            for (int i = 0; i < 20; i++)
+            {
+                do
+                {
+                    piezaActual = rnd.Next(1, 8);
+                    if (piezaActual == piezaAnterior) seRepite = true;
+                    else seRepite = false;
+                } while (seRepite);
+                piezaAnterior = piezaActual;
+                Console.WriteLine(piezaActual);
+            }
+        }
+    }
+}
