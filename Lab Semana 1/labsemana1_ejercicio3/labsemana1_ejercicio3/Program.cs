0b091033159ea85391d1220691f899febb7178ea
vis-hoo <vicente.falcon.arancibia@hotmail.com>
2023-03-18 10:02:33 -0300

    Lab 1 - Ejercicio 3 - C#

---
+++ Lab Semana 1/labsemana1_ejercicio3/labsemana1_ejercicio3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace labsemana1_ejercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string unicodeCaracter = "\u2588";
+
+            Console.WriteLine("\n\tPresione Enter para comenzar a cargar.");
+            Console.ReadLine();
+            for (int i = 0; i < 37; i++)
+            {
+                Thread.Sleep(300);
+                if (i == 0) Console.Write("\t" + unicodeCaracter);
+                Console.Write(unicodeCaracter);
+            }
+            Console.WriteLine("\n\n");
+        }
+    }
+}
