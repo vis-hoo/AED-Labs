b8a961744abb7f90246926812c50edcbcd6b8ccc
vis-hoo <vicente.falcon.arancibia@hotmail.com>
2023-03-18 10:00:16 -0300

    Lab 1 - Ejercicio 2 - C#

---
+++ Lab Semana 1/labsemana1_ejercicio2/labsemana1_ejercicio2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labsemana1_ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Mi número es el 7.\nIngresa tu número: ");
+            double numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nSuma: 7 + " + numero + " = " + (7 + numero) + ".");
+            Console.WriteLine("Resta: 7 - " + numero + " = " + (7 - numero) + ".");
+            Console.WriteLine("Multiplicación: 7 * " + numero + " = " + (7 * numero) + ".");
+            Console.WriteLine("División: 7 / " + numero + " = " + (7 / numero) + ".\n");
+        }
+    }
+}
