9c5b5cf2241fa675a9f848601fb26e92497cbd5b
vis-hoo <vicente.falcon.arancibia@hotmail.com>
2023-03-18 09:57:03 -0300

    Lab 1 - Ejercicio 1 - C#

---
+++ Lab Semana 1/labsemana1_ejercicio1/labsemana1_ejercicio1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labsemana1_ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int entero = 15;
+            float decimal_ = 0.09f;
+            bool logico = false;
+            string texto = "Laboratorio";
+
+            Console.WriteLine("Int guarda datos de tipo entero: " + entero +
+                ".\nFloat guarda datos de tipo decimal: " + decimal_ +
+                ".\nBool guarda datos de tipo lógico: " + logico +
+                ".\nString guarda texto: " + texto + ".\n");
+        }
+    }
+}
