0ecce6035497a69d555a141380558a40be7175cc
vis-hoo <vicente.falcon.arancibia@hotmail.com>
2023-03-18 10:15:49 -0300

    Lab 1 - Ejercicio 7 - C#

---
+++ Lab Semana 1/labsemana1_ejercicio7/labsemana1_ejercicio7/Auto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labsemana1_ejercicio7
+{
+    internal class Auto
+    {
+        string Fabricante;
+        string Modelo;
+        int Año;
+        int Velocidad;
+        int Maniobrabilidad;
+
+        public Auto(string fabricante, string modelo, int año, int velocidad, int maniobrabilidad)
+        {
+            Fabricante = fabricante;
+            Modelo = modelo;
+            Año = año;
+            Velocidad = velocidad;
+            Maniobrabilidad = maniobrabilidad;
+        }
+
+        public void ImprimirInfo()
+        {
+            Console.Write("\n\tFabricante: " + Fabricante +
+                ".\n\tModelo: " + Modelo +
+                ".\n\tAño: " + Año +
+                ".\n\tVelocidad: " + Velocidad +
+                "%.\n\tManiobrabilidad: " + Maniobrabilidad + "%.\n\n");
+        }
+    }
+}
---
+++ Lab Semana 1/labsemana1_ejercicio7/labsemana1_ejercicio7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labsemana1_ejercicio7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto auto1 = new Auto("Volkswagen", "Voyage VW", 2020, 75, 80);
+            Auto auto2 = new Auto("Toyota", "RAV4", 2019, 65, 95);
+            Auto auto3 = new Auto("Nissan", "Qashqai", 2021, 90, 70);
+
+            auto1.ImprimirInfo();
+            auto2.ImprimirInfo();
+            auto3.ImprimirInfo();
+        }
+    }
+}
